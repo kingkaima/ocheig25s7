49b9b7d52221c057be439f7a8ce7848880e752f8
kingkaima <princekaimad1st@gmail.com>
2025-07-11 15:12:38 -0400

    Assignment4.0-index.html
    Created new arrays to the code and assigned  a varaiable newStory = storyText;

---
+++ Assignment4.0/main.js
@@ -0,0 +1,55 @@
+console.log("part1");
+
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day"
+
+let insertx = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
+];
+
+let inserty = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House",
+
+];
+
+let insertz = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+     "turned into a slug and crawled away",
+];
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
