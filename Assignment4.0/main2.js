bc6855b938c98532d14128a8e2a52fb05d3d2f4e
kingkaima <princekaimad1st@gmail.com>
2025-07-18 15:18:59 -0400

    Assignment4.0-index2.html
    In this part of the assignment i created an image folder to  store all five images and at the same time.stored them as consatnts in my main2.js javascript file.

---
+++ Assignment4.0/main2.js
@@ -0,0 +1,10 @@
+const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
+
+const altText ={
+    'pic1.jpg':'Close-up of a big human-eye',
+    'pic2.jpg':'Macaw closeup',
+    'pic3.jpg':'Orange and black butterfly',
+    'pic4.jpg':'purple pansy',
+    'pic5.jpg':'pineapple',
+
+};
