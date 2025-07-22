4feff763d13a4b1b5dc60d5044eaa063a5a6299b
kingkaima <princekaimad1st@gmail.com>
2025-07-22 11:01:03 -0400

    Assignment4.0-index2.html
    I declared as const images = ['pic1.jpg', ..., 'pic5.jpg']; to store the filenames used in the gallery.
    
     And i added a const altText object was added to map each image filename to a more descriptive alt attribute (e.g., 'pic1.jpg': 'Close-up of a big human-eye').
    
    thumbBar and displaying were defined to reference the thumbnail bar and the main displayed image.
    
    Loop to Generate Thumbnails:
    
    A for loop that dynamically creates <img> elements for each thumbnail using the images array.
    
    Sets the src and alt for each image using the filename and altText object.
    
    Click Event Listeners:
    
    Each thumbnail image gets a click event that updates the main displayed image's src and alt.

--- Assignment4.0/main2.js
+++ Assignment4.0/main2.js
@@ -8,3 +8,23 @@ const altText ={
     'pic5.jpg':'pineapple',
 
 };
+
+//Get's references for elements I will use
+const thumbBar = document.querySelector('.thumb bar');
+const displaying = document.querySelector('.displayed-img')
+
+//Loop's through the thumbnails and creates filenames with click listeners
+for(let i=0;i< imahes.length;i++) {
+const newImage = document.createElement('img');
+newImage.src = 'images/' + images[i];
+newImage.alt = altText[images[i]];
+
+// Add Click event listener to display updated image
+newImage.addEventListener('click', function() {
+    displayedImg.src = this.src;  // Set main image src to thumbnail src
+    displayedImg.alt = this.alt;  // Set main image alt to thumbnail alt
+  });
+
+  // Add the new thumbnail to the thumbBar div
+  thumbBar.appendChild(newImage);
+}
