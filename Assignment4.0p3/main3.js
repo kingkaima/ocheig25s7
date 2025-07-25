e74f6fff712233aa0f0627eb1e41afd87ece9d53
kingkaima <princekaimad1st@gmail.com>
2025-07-25 15:20:01 -0400

    Assignment4.0p3/index3.html2
    I allowed or created interractivity to test the ball.And ensure drawing logic works before animation and collision

--- Assignment4.0p3/main3.js
+++ Assignment4.0p3/main3.js
@@ -17,3 +17,45 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update(canvasWidth, canvasHeight) {
+    if ((this.x + this.size) >= canvasWidth || (this.x - this.size) <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if ((this.y + this.size) >= canvasHeight || (this.y - this.size) <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomRGB() {
+  const r = random(0, 255);
+  const g = random(0, 255);
+  const b = random(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
